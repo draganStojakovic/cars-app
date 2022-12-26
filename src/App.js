9d7a1021fa3dcd3e1eb04a3955395fada36c1d86
qouillyan <stojakovicdragan98@gmail.com>
2022-12-26 20:56:04 +0100

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { AppCars } from "./components/AppCars";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <Link to="/cars"> Cars </Link>
+        </nav>
+        <Switch>
+          <Route path="/cars">
+            <AppCars />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/AppCars.jsx
@@ -0,0 +1,3 @@
+export const AppCars = () => {
+  return <p>App Cars</p>
+};
